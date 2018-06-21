701719e081edeb51dcbab768ab7135da227fa431
GZD9L4@ALLYIPOC.COM <GZD9L4@ALLYIPOC.COM>
2018-06-21 11:16:22 -0400

    new model and invoice class

--- src/main/java/com/invocify/Invoices/models/Company.java
+++ src/main/java/com/invocify/Invoices/models/Company.java
@@ -1,5 +1,44 @@
 package com.invocify.Invoices.models;
 
-public class Company {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="Company")
+public class Company {
+	 @Id
+	 @GeneratedValue
+	 private int id;
+	 private String name;
+
+	 @OneToMany(mappedBy="Company")
+	 private Invoice invoice;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+	 
 }
--- src/main/java/com/invocify/Invoices/models/Invoice.java
+++ src/main/java/com/invocify/Invoices/models/Invoice.java
@@ -1,5 +1,32 @@
 package com.invocify.Invoices.models;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Invoice")
+
 public class Invoice {
+	
+	@Id
+	@GeneratedValue
+	private int id; 
+	
+	public int createdOn;
+	public String invoiceDescription; 
+	
+	@OneToMany(mappedBy="invoices", cascade=CascadeType.ALL)
+	
+	
+	@ManyToOne
+	Company company; 
+	
+	
+	
 
 }
